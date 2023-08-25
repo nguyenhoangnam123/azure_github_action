a864c49426210d6451a3dba8aa767c1423564948
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-26 00:58:03 +0700

    use OIDC authentication and authorization for backend and provider

--- infras/env/1.dev/versions.tf
+++ infras/env/1.dev/versions.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state"
-    storage_account_name = "namnh21894"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    use_oidc = true
+    #    resource_group_name  = "terraform-state"
+    #    storage_account_name = "namnh21894"
+    #    container_name       = "tfstate"
+    #    key                  = "dev.terraform.tfstate"
     #    use_azuread_auth     = true # should use Azure AD to access storage account
     #    subscription_id      = "0081ea25-c945-486a-b7c6-6fe8c1bd4169"
     #    tenant_id            = "b5606dd5-4171-4134-a6bd-ec46618ad53d"
@@ -22,7 +23,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-
+  use_oidc = true
   #  subscription_id = "0081ea25-c945-486a-b7c6-6fe8c1bd4169"
   #  tenant_id       = "b5606dd5-4171-4134-a6bd-ec46618ad53d"
   #  client_id       = "23e114c5-6fb1-4f37-b733-e8b545f75bc7" # service principal id
