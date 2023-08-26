f2fe5d9e48f8b7bc5d2bd3c57c1ff64d27f597b4
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-26 14:33:28 +0700

    remove inline var and add key to backend config

--- infras/env/1.dev/versions.tf
+++ infras/env/1.dev/versions.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
     use_oidc = true
+    key      = "dev.terraform.tfstate"
     #    resource_group_name  = "terraform-state"
     #    storage_account_name = "namnh21894"
     #    container_name       = "tfstate"
-    #    key                  = "dev.terraform.tfstate"
     #    use_azuread_auth     = true # should use Azure AD to access storage account
     #    subscription_id      = "0081ea25-c945-486a-b7c6-6fe8c1bd4169"
     #    tenant_id            = "b5606dd5-4171-4134-a6bd-ec46618ad53d"
