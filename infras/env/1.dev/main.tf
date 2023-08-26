287d53d949e4cd1bbdb7a41e8a14f91eae90c1dd
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-27 05:09:37 +0700

    rename runbook

--- infras/env/1.dev/main.tf
+++ infras/env/1.dev/main.tf
@@ -292,7 +292,7 @@ resource "azurerm_automation_runbook" "query_database_rbac" {
 }
 
 resource "azurerm_automation_webhook" "query_database_rbac_runbook_webhook" {
-  name                    = "TestRunbook_webhook"
+  name                    = "${local.prefix}-query-database-webhook"
   resource_group_name     = azurerm_resource_group.dev_rg.name
   automation_account_name = azurerm_automation_account.main.name
   expiry_time             = "2023-12-31T00:00:00Z"
