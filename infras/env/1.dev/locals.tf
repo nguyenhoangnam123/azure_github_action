43fc2fdda25f1124704190630605ecc510b50cb4
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-27 06:16:07 +0700

    fix locals key_vault_access_policies

--- infras/env/1.dev/locals.tf
+++ infras/env/1.dev/locals.tf
@@ -56,8 +56,8 @@ locals {
   ])
 
   key_vault_access_policies = setunion(
-    key_vault_user_access_policies,
-    key_vault_sps_access_policies
+    local.key_vault_user_access_policies,
+    local.key_vault_sps_access_policies
   )
 
   common_tags = {
