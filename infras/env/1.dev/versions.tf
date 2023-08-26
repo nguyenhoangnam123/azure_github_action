8736fda8de6be13a9a5202027a49d446bf5dcda5
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-26 14:42:35 +0700

    add azuread provider for azure sp

--- infras/env/1.dev/versions.tf
+++ infras/env/1.dev/versions.tf
@@ -1,4 +1,18 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.70.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.41.0"
+    }
+  }
   backend "azurerm" {
     use_oidc = true
     key      = "dev.terraform.tfstate"
@@ -9,16 +23,6 @@ terraform {
     #    subscription_id      = "0081ea25-c945-486a-b7c6-6fe8c1bd4169"
     #    tenant_id            = "b5606dd5-4171-4134-a6bd-ec46618ad53d"
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.70.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-  }
 }
 
 provider "azurerm" {
@@ -27,4 +31,7 @@ provider "azurerm" {
   #  subscription_id = "0081ea25-c945-486a-b7c6-6fe8c1bd4169"
   #  tenant_id       = "b5606dd5-4171-4134-a6bd-ec46618ad53d"
   #  client_id       = "23e114c5-6fb1-4f37-b733-e8b545f75bc7" # service principal id
+}
+
+provider "azuread" {
 }
