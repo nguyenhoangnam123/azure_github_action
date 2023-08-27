64d3b8a80eeb81a41bf42bd0476d642d9ef81be9
nguyenhoangnam123 <namnh21894@gmail.com>
2023-08-27 07:07:44 +0700

    fix key vault access

--- infras/env/1.dev/main.tf
+++ infras/env/1.dev/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_key_vault" "key_vault" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
-  access_policy = {
+  access_policy {
       tenant_id = data.azurerm_client_config.current.tenant_id
       object_id = data.azurerm_client_config.current.object_id
 
